24c86c4ebebf4811afc360399c5f9934e1cf5a6f
nininhosam <luizfelipeforc@gmail.com>
2022-11-28 01:26:02 -0300

    Added/Hover for description, pt_br (default) && en

--- index.js
+++ index.js
@@ -2,6 +2,27 @@ const dog = document.querySelector("img#dog")
 const main = document.querySelector("main")
 const ui = document.querySelector("img#ui")
 const overlay = document.querySelector("div#overlay")
+const description = document.querySelector("p#description")
+const setLanguage = lg => lang = lg;
+const pt_br = {
+    "headphones": "Um headphone comum.",
+    "shirt": "Uma camiseta comum.",
+    "pants": "Uma calça comum.",
+    "body": "Sou eu",
+    "dog": "Tequila!",
+    "pt_br": "[Selecionado]",
+    "en": "Change to English?"
+}
+const en = {
+    "headphones": "Just a pair of headphones.",
+    "shirt": "Just an ordinary shirt.",
+    "pants": "Just an ordinary pair of pants.",
+    "body": "That's me",
+    "dog": "Tequila!",
+    "pt_br": "Mudar para português?",
+    "en": "[Selected]"
+}
+var lang = pt_br
 dog.addEventListener("click", () => {
     const popup = document.createElement("img")
     popup.setAttribute("src", "./assets/dog_bg.png")
@@ -31,3 +52,11 @@ function off() {
     popup.remove()
     dogHead.remove()
 }
+document.querySelectorAll(".interactive").forEach(element => {
+    element.addEventListener("mousemove", ()=>{
+        description.innerHTML = lang[element.id]
+    })
+    element.addEventListener("mouseout", ()=>{
+        description.innerHTML = ""
+    })
+});
